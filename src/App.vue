d97610670c9505c5af22e8078e1165a23d05a324
Chaitanya Joshi <ckjoshi9@gmail.com>
2017-10-25 19:56:02 +0800

    can store value  and use to process money stuff

--- src/App.vue
+++ src/App.vue
@@ -9,15 +9,22 @@
       <form id="form" v-on:submit.prevent="addProfile">
         <div class="form-group">
           <div v-for="university in Universities">
-            <input type="radio" :id="university.Name" :value="university.Name" v-model="newProfile.University">
+            <input type="radio" :id="university.Name" :value="university" v-model="newProfile.University">
             <label :for="university.Name">{{university.Name}}</label>
           </div>
         </div>
         <hr>
         <div class="form-group">
-          <div v-for="hall in Halls" v-if="hall.University==newProfile.University">
-            <input type="radio" :id="hall.HallName" :value="hall.HallName" v-model="newProfile.Hall">
-            <label :for="hall.HallName">{{hall.HallName}}</label>
+          <div v-for="hall in Halls" v-if="hall.University==newProfile.University.Name">
+            <input type="radio" :id="hall.Name" :value="hall" v-model="newProfile.Hall">
+            <label :for="hall.Name">{{hall.Name}}</label>
+          </div>
+        </div>
+        <hr>
+        <div class="form-group">
+          <div v-for="food in Foods" v-if="food.University==newProfile.University.Name">
+            <input type="radio" :id="food.Type" :value="food" v-model="newProfile.Food">
+            <label :for="food.Type">{{food.Type}}</label>
           </div>
         </div>
         <hr>
@@ -25,17 +32,21 @@
       </form>
     </div>
 
-    <div class="container">
+    <hr>
+
+    <!-- <div class="container">
       <div v-for="entry in Profiles">
           <div class="col-md-4 col-md-offset-4">
-            <button class="btn btn-default" v-on:click="removeProfile(entry)" >{{entry.University}},{{entry.Hall}}</button>
+            <button class="btn btn-default" v-on:click="removeProfile(entry)" >{{entry.University}},{{entry.Hall}},{{entry.Food}}</button>
           </div>
       </div>
-    </div>
+    </div> -->
 
-    <div>
-      {{newProfile.University}}
+    <div class="col-md-12 text-center">
+<!--       {{newProfile.University}}
       {{newProfile.Hall}}
+      {{newProfile.Food}} -->
+      {{computedValue}}
     </div>
 
   </div>
@@ -58,32 +69,31 @@ let config = {
 let app = Firebase.initializeApp(config)
 let db = app.database()
 
-let ProfilesRef = db.ref('1/Profiles')
-let UniversitiesRef = db.ref('2/Universities')
-let HallsRef = db.ref('3/Halls')
+let ProfilesRef = db.ref('Profiles')
+let UniversitiesRef = db.ref('Universities')
+let HallsRef = db.ref('Halls')
+let FoodsRef = db.ref('Foods')
 
 export default {
   name: 'app',
   
-
   data () {
     return {
-      u: '',
-      h: '',
       newProfile: {
-          University : '',
-          Hall : ''
-      }
+          University : null,
+          Hall : null,
+          Food : null
+      },
     }
   },
 
   methods: {
     addProfile: function () {
-      ProfilesRef.push(this.newProfile)
-      this.newProfile.University = '';
-      this.newProfile.Hall = '';
+      //ProfilesRef.push(this.newProfile)
+      this.newProfile.University = null;
+      this.newProfile.Hall = null;
+      this.newProfile.Food = null;
       console.log('New Entry added')
-      location.reload()
     },
     removeProfile: function (entry) {
       ProfilesRef.child(entry['.key']).remove()
@@ -93,19 +103,28 @@ export default {
   computed: {
     reversedHalls: function () {
       return this.Halls.reverse()
+    },
+    computedValue: function() {
+      let u = (this.newProfile.University == null) ? 0 : this.newProfile.University.Fee;
+      let h = (this.newProfile.Hall == null) ? 0 : this.newProfile.Hall.Rent;
+      let f = (this.newProfile.Food == null) ? 0 : this.newProfile.Food.Price;
+      let day = [u/365, h/30, f];
+      let week = [u/56, h/4, f*7]
+      let month = [u/12, h, f*30];
+      let year = [u, h*12, f*56];
+      return [day, week, month, year];
     }
   },
   components: {
 
   },
   firebase: {
+    Profiles: ProfilesRef,
     Universities: UniversitiesRef,
     Halls: HallsRef,
-    Profiles: ProfilesRef
+    Foods: FoodsRef
   },
 }
-
-
 </script>
 
 
